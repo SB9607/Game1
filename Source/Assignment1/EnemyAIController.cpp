47597a06d8ba610993e388e3b5d454a0cfdaadaf
Simon <simonball96@gmail.com>
2022-08-05 15:50:56 +0100

    Added basic AI functionality

--- Source/Assignment1/EnemyAIController.cpp
+++ Source/Assignment1/EnemyAIController.cpp
@@ -3,13 +3,58 @@
 
 #include "EnemyAIController.h"
 #include "Kismet/GameplayStatics.h"
+#include "Engine/TargetPoint.h"
 
 void AEnemyAIController::BeginPlay() 
 {
 	Super::BeginPlay();
 
-	APawn* PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
-	SetFocus(PlayerPawn);
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ATargetPoint::StaticClass(), Waypoints);
 
-	MoveToActor(PlayerPawn, 10.0f);
+	RandomPatrol();
+
+	PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0);
 }
+
+// Called every frame
+void AEnemyAIController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	APawn* AIPawn = GetPawn();
+	FVector AIForwardVector = AIPawn->GetActorForwardVector(); //already nomalised
+	FVector PlayerPositionVector = PlayerPawn->GetActorLocation();
+	FVector AIPositionVector = AIPawn->GetActorLocation();
+	FVector AIToPlayerVector = PlayerPositionVector - AIPositionVector;
+	AIToPlayerVector.Normalize(); //this vector must be explicitly normalised
+	float DirectionDotProduct = FVector::DotProduct(AIToPlayerVector, AIForwardVector);
+	UE_LOG(LogTemp, Warning, TEXT("dot product is: %f"), DirectionDotProduct);
+
+	if (LineOfSightTo(PlayerPawn) && DirectionDotProduct >= 0)
+	{
+		SetFocus(PlayerPawn);
+		MoveToActor(PlayerPawn, 10.0f);
+	}
+}
+
+void AEnemyAIController::OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result)
+{
+	Super::OnMoveCompleted(RequestID, Result);
+	PlaceToMoveTo = nullptr;
+	GetWorld()->GetTimerManager().SetTimer(WaitTimer, this, &AEnemyAIController::RandomPatrol, WaitDuration, false);
+}
+
+
+AActor* AEnemyAIController::ChooseWaypoint()
+{
+	int index = FMath::RandRange(0, Waypoints.Num() - 1);
+	return Waypoints[index];
+}
+
+void AEnemyAIController::RandomPatrol()
+{
+	PlaceToMoveTo = ChooseWaypoint();
+	SetFocus(PlaceToMoveTo);
+	MoveToActor(PlaceToMoveTo, 10.0f);
+}
+
--- Source/Assignment1/EnemyAIController.h
+++ Source/Assignment1/EnemyAIController.h
@@ -14,7 +14,33 @@ class ASSIGNMENT1_API AEnemyAIController : public AAIController
 {
 	GENERATED_BODY()
 	
+public:
+	void OnMoveCompleted(FAIRequestID RequestID, const FPathFollowingResult& Result) override;
+
+	UFUNCTION()
+	void RandomPatrol();
+
+	FTimerHandle WaitTimer;
+
 protected:
 	virtual void BeginPlay() override;
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+private:
+	UPROPERTY()
+	TArray<AActor*> Waypoints;
+
+	UFUNCTION()
+	AActor* ChooseWaypoint();
+
+	UPROPERTY(EditAnywhere)
+	float WaitDuration = 3.0f;
+
+	APawn* PlayerPawn;
+
+	AActor* PlaceToMoveTo;
 
 };
